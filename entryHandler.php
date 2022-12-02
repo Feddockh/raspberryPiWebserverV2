0829aada0cc4c739b5268d7d60aa2fd48a1b3617
feddockh <hayden4feddock@gmail.com>
2022-12-02 14:25:24 -0500

    background python script added which is now responsible for maintaining the flow of data from the serial port, updating and moving data in the SQL tables, and updating the a json file with the live player and time. entry handler was also updated to add users to the json file and the queue now. began updating jquery script in the index to pull data from the json file instead of calling php to fetch data from the sql db. This new system will be a lot faster and will protect against overloading the serial port or SQL db with requests.

--- entryHandler.php
+++ entryHandler.php
@@ -1,21 +1,50 @@
 <?php
-    // Server parameters
-    $servername = "localhost";
-    $username = "vce";
-    $password = "Volvo1927";
-    $dbname = "vce";
 
-    // Establish connection
-    $connection = new mysqli($servername, $username, $password, $dbname);
+    $json_file = 'webdata/data.json';
 
     // Check if there are new names to be added
     if (array_key_exists('username', $_POST)) {
 
+        // Server parameters
+        $servername = "localhost";
+        $username = "vce";
+        $password = "Volvo1927";
+        $dbname = "vce";
+
+        // Establish connection
+        $connection = new mysqli($servername, $username, $password, $dbname);
+
         // TODO: Check if name already exists
+
+        // TODO: Make sure that name is valid and not dangerous to SQL
+
+        // Return all rows from the queue table
+        $sql = "SELECT * FROM queue";
+        $result = $connection->query($sql);
+
+        // If the number of players in the queue is empty, update the json file first
+        if ($result->num_rows == 0) {
+
+            // Get and decode the data from the json file
+            $json_string = file_get_contents($json_file);
+            $playerInfo = json_decode($json_string, true);
+
+            // Update the username
+            $playerInfo['username'] = $_POST['username'];
+
+            // Encode and put the data back into the json file
+            $json_string = json_encode($playerInfo, JSON_PRETTY_PRINT);
+            file_put_contents($json_file, $json_string);
+        }
+
+        // Insert new player into queue 
+        //$sql = "INSERT INTO queue (username) 
+        //    VALUES ('" . $_POST['username'] . "')"; // Formatting odd, had to pass as strings using '' inside of the ""
+        //$connection->query($sql);
         
-        // Insert data into table
-        $sql = "INSERT INTO queue (username, time) 
-            VALUES ('" . $_POST['username'] . "', '" . '0:00' . "')"; // Formatting odd, had to pass as strings using '' inside of the ""
-        $connection->query($sql);
+        $connection->close();
     }
+
+
+
 ?>
